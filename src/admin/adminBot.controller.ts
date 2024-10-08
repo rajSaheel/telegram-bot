b90576b12f09b276d1b9aaf79b16fa8034df4d9b
rajSaheel <srg12114@gmail.com>
2024-10-08 20:10:51 +0530

    debugging -v1

--- src/admin/adminBot.controller.ts
+++ src/admin/adminBot.controller.ts
@@ -1,22 +1,17 @@
-import { Body, Controller, Get, Param, Patch } from "@nestjs/common";
-import { AdminBotService } from "./adminBot.service";
+import { Body, Controller, Get, Param, Patch } from '@nestjs/common';
+import { AdminBotService } from './adminBot.service';
 
-@Controller("botsettings")
-export class AdminBotController{
-    constructor (private readonly botService:AdminBotService){}
+@Controller('botsettings')
+export class AdminBotController {
+  constructor(private readonly botService: AdminBotService) {}
 
-    @Get("hello")
-    sayHello(){
-        return {message:"Hello",status:"Success"}
-    }
+  @Get()
+  getBotSettings() {
+    return this.botService.getBotSettings();
+  }
 
-    @Get()
-    getBotSettings(){
-        return this.botService.getBotSettings()
-    }
-
-    @Patch()
-    updateBotSettings(@Body()body){
-        return this.botService.updateBotSettings(body)
-    }
+  @Patch()
+  updateBotSettings(@Body() body) {
+    return this.botService.updateBotSettings(body);
+  }
 }
